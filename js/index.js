fca5ca96562cd75666c78acbc382aabd8df2d669
TolikaTolika123 <91464584+TolikaTolika123@users.noreply.github.com>
2022-06-18 13:36:43 +0400

    added responsieveness except skills list

--- js/index.js
+++ js/index.js
@@ -1,12 +1,36 @@
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 const header = document.querySelector('header')
+const menuOpenBtn = document.querySelector('.navigation__btn')
+const menuCloseBtn = document.querySelector('.navigation__close')
+const navigationList = document.querySelector('.navigation__list')
+const headerTitle = document.querySelector('.header__title')
+const navigationLinks = document.querySelectorAll('.navigation__link')
+
+// navigation menu toggle
+
+
+const showMenu = () => {
+  navigationList.classList.add('active')
+  headerTitle.classList.add('black')
+}
+
+const hideMenu = () => {
+  navigationList.classList.remove('active')
+  headerTitle.classList.remove('black')
+}
+
+menuOpenBtn.addEventListener('click', showMenu)
+menuCloseBtn.addEventListener('click', hideMenu)
+navigationLinks.forEach(navLink => navLink.addEventListener('click', hideMenu))
 
 // changing header text color after user reaches about section
 window.onscroll = function() {
   if (window.scrollY >= vh - 30) {
     header.classList.add('active');
+    menuOpenBtn.classList.add('black');
   } else {
     header.classList.remove('active');
+    menuOpenBtn.classList.remove('black');
   }
 };
 
